969a2d3376f5718bb2edb40101e711765d54a902
KamilBalawender73113 <73113@student.wsiz.edu.pl>
2025-11-07 10:55:59 +0100

    .

---
+++ zad2.py
@@ -0,0 +1,22 @@
+x = float(input("Podaj liczbę x:"))
+y = float(input("Podaj liczbę y:"))
+z = float(input("Podaj liczbę z:"))
+
+def zad2(x,y,z):
+    if x>=y and x >=z:
+        if y>=z:
+            print(z,y,a)
+        else:
+            print(y,z,x)
+    elif y>=x and y >=z:
+        if z>=x:
+            print(z,x,y)
+        else:
+            print(x,z,y)
+    elif z>=x and z >= y:
+        if x>=y:
+            print(y,x,z)
+        else:
+            print(x,y,z)
+
+print(zad2(x,y,z))
