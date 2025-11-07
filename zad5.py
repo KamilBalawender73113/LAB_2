8f3f87703c61442bb7e3769632ad5c04878be306
KamilBalawender73113 <73113@student.wsiz.edu.pl>
2025-11-07 14:30:59 +0100

    .

--- zad5.py
+++ zad5.py
@@ -1,2 +1,3 @@
 with open("notowania_gieldowe.txt", "r") as plik:
     print(plik.read())
+    
---
+++ zad5B.py
@@ -0,0 +1,4 @@
+with open("notowania_gieldowe.txt", "a") as plik:
+    plik.write("\nALR, 113")
+with open("notowania_gieldowe.txt", "r") as plik:
+    print(plik.read())
