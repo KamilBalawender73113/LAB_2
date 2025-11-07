c4448fadf1c967e42aabbfba4090b004f3b18bce
KamilBalawender73113 <73113@student.wsiz.edu.pl>
2025-11-07 10:53:50 +0100

    .

---
+++ zad1.py
@@ -0,0 +1,8 @@
+punkty = float(input("Podaj liczbę zdobytych punktów:"))
+if punkty  > 80:
+    print("Egzamin zdany")
+elif punkty > 50:
+    if punkty <80:
+        print("Można poprawic (nieobowiązkowe)")
+elif punkty < 50:
+    print("Musisz poprawić egzamin")
